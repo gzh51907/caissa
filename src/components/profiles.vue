385509d17532f5c9904d7cce5560fccaa6a13ea3
yourname <52962998+TJF-wind@users.noreply.github.com>
2019-10-12 17:57:29 +0800

    first

---
+++ src/components/profiles.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+
+        <bookinf>
+        </bookinf>
+        <commoninf></commoninf>
+        <detailinf></detailinf>
+        <inquiryinf></inquiryinf>
+        <navjump></navjump>
+
+    </div>
+</template>
+<script>
+    import bookinf from '../pages/book_inf';
+    import commoninf from '../pages/common_inf';
+    import detailinf from '../pages/detail_inf';
+    import inquiryinf from '../pages/inquiry_inf';
+    import navjump from '../pages/navjump';
+
+export default {
+    components:{
+    bookinf,
+    commoninf,
+    detailinf,
+    inquiryinf,
+    navjump
+    }
+}
+</script>
+<style >
+el-row{
+        height: 100%;
+        }
+
+    el-footer{
+        position: fixed;
+        width: 100%;
+        left: 0;
+        bottom: 0;
+        z-index: 1000;
+
+    }
+</style>
---
+++ src/pages/book_inf.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <el-card class="box-card">
+            <el-row>
+                <el-col :span="24"><div class="grid-content bg-purple-dark">
+                    <i class="el-icon-setting" style="float:right;font-size:30px;color:#ddd;height:30px"></i>
+                    </div></el-col>
+            </el-row>
+
+            <el-row>
+                <el-col :span="8">
+                    <div class="block"><el-avatar shape="circle" :size="30" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar></div>
+                </el-col>
+                <el-col :span="16"><div class="grid-content bg-purple">
+                    <el-button type="text">文字按钮</el-button>
+                    </div></el-col>
+            </el-row>
+        </el-card>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    .box-card{
+    height: 133px;
+    background-image: url(http://m.caissa.com.cn/static/images/percentre/header_bg.png);
+}
+</style>
