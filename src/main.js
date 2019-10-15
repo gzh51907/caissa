ebd74e7bd12198ed1d01023a2f85a52b8fa0ac6c
Blijinhui <1455098147@qq.com>
2019-10-15 11:24:25 +0800

    ljh修改package-lock1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false
 import router from './routers/ljh_index.js';
-import router from './routers/tjfindex';
+// import router from './routers/tjfindex';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
